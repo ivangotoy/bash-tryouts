601bfd9c04025cc4230ef3beb287e6e084b8cb88
Ivan Angelov <ivangotoy@gmail.com>
2021-08-27 18:40:03 +0300

    blacklist.sh works better now and easier to use.

--- cron/blacklist.sh
+++ cron/blacklist.sh
@@ -7,19 +7,28 @@
 # 
 # Tooling used:
 # 
-# - rg nginx ipset
+# - rg nginx ipset iptables
 # 
 # Works nicely in crontab, just keep your nginx.log populated at all times :)
 #
-# Feel free to create a hashtable called blacklist in ipset too :)
+# Feel free to create a hashtable called blacklist in ipset too... or bother to read below.
 #
+# Create ipset hashtable/set called "blacklist" with this command:
+#
+# ipset create (-N is like create too) blacklist hash:ip
+#
+# Make iptables do the right thing with your newly created hashtable called "blacklist":
+#
+# iptables -I INPUT -m set --match-set blacklist src -j DROP
+#
+# Save you iptables rules and populate your blacklist hastabled.
 
 export LC_ALL=C
 export LANG=C
 
 log="/var/log/nginx/access.log"
 myip="127.0.0.1|192.168.1.1|192.168.1.222|78.130.248.61|192.168.1.70"
-list="$(rg 'Nikto|/\.env |nuclei|union|UNION|sql|conf|/[(w|x)]*shell|cmd|Admin|admin|cfg' "$log" | rg -v "$myip|DuckDuckBot|Adsbot|Googlebot|YandexBot|bingbot|SemrushBot|PetalBot" | cut -d ' ' -f 1,9 | rg '301|302|400|403|404|499' | cut -d ' ' -f 1 | sort --parallel=4 -n | uniq -i)"
+list="$(rg 'wp-login.php|logon|getuser|execute|passwd|Nikto|/\.env |nuclei|union|UNION|sql|conf|/[(w|x)]*shell|cmd|Admin|admin|cfg|boaform' "$log" | rg -v "$myip|DuckDuckBot|Adsbot|Googlebot|YandexBot|bingbot|SemrushBot|PetalBot" | cut -d ' ' -f 1,9 | rg '301|302|400|403|404|499' | cut -d ' ' -f 1 | sort --parallel=4 -n | uniq -i)"
 
 ipset -q restore -! < /etc/ipsets.conf &> /dev/null
 ipset -q flush blacklist &> /dev/null
