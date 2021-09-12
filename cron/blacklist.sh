13940ed6f4ce6a292651d106d13953de8e919b4a
ivangotoy <ivangotoy@gmail.com>
2021-09-12 16:08:08 +0300

    blacklist.sh more strings added for bad behavior detection

--- cron/blacklist.sh
+++ cron/blacklist.sh
@@ -21,20 +21,22 @@
 #
 # iptables -I INPUT -m set --match-set blacklist src -j DROP
 #
-# Save you iptables rules and populate your blacklist hastabled.
+# Save you iptables rules and populate your blacklist hastable.
 
 export LC_ALL=C
 export LANG=C
 
 log="/var/log/nginx/access.log"
 myip="127.0.0.1|192.168.1.1|192.168.1.222|78.130.248.61|192.168.1.70"
-list="$(rg 'main.installer.php|\?author=[0-9]|``wp-login.php|logon|getuser|execute|passwd|Nikto|/\.env |nuclei|union|UNION|sql|conf|/[(w|x)]*shell|cmd|Admin|admin|cfg|boaform' "$log" | rg -v "$myip|DuckDuckBot|Adsbot|Googlebot|YandexBot|bingbot|SemrushBot|PetalBot" | cut -d ' ' -f 1,9 | rg '301|302|400|403|404|499' | cut -d ' ' -f 1 | sort --parallel=4 -n | uniq -i)"
+list="$(rg 'content-post.php|mide.php|phpunit|HEADER.md|/owa/auth/logon.aspx|main.installer.php|\?author=[0-9]|wp-login.php|logon|getuser|execute|passwd|Nikto|/\.env |nuclei|union|UNION|sql|conf|/[(w|x)]*shell|cmd|Admin|admin|cfg|boaform' "$log" | rg -v "$myip|DuckDuckBot|Adsbot|Googlebot|YandexBot|bingbot|SemrushBot|PetalBot" | cut -d ' ' -f 1,9 | rg '301|302|400|403|404|499' | cut -d ' ' -f 1 | sort --parallel=4 -n | uniq -i)"
 
 ipset -q restore -! < /etc/ipsets.conf &> /dev/null
+
 for badip in $list
 do
 ipset -q -A blacklist "$badip"
 done
 
 ipset -q save blacklist -f /etc/ipsets.conf &> /dev/null
+
 printf "Cron status OK - $(date)\n" >> /home/$USER/blacklist/status
