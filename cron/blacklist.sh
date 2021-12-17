f0c9b3876d196f5bfa4eea344209d715b95b05ad
ivangotoy <ivangotoy@gmail.com>
2021-12-17 23:24:00 +0200

    better blacklist regex matching jndi

--- cron/blacklist.sh
+++ cron/blacklist.sh
@@ -29,8 +29,8 @@ export LC_ALL=C
 export LANG=C
 
 log="/var/log/nginx/access.log"
-myip="127.0.0.1|192.168.1.1|192.168.1.222|78.130.248.61|192.168.1.70"
-list="$(rg 'content-post.php|mide.php|phpunit|HEADER.md|/owa/auth/logon.aspx|main.installer.php|\?author=[0-9]|wp-login.php|logon|getuser|execute|passwd|Nikto|/\.env |nuclei|union|UNION|sql|conf|/[(w|x)]*shell|cmd|Admin|admin|cfg|boaform' "$log" | rg -v "$myip|DuckDuckBot|Adsbot|Googlebot|YandexBot|bingbot|SemrushBot|PetalBot" | cut -d ' ' -f 1,9 | rg '301|302|400|403|404|499' | cut -d ' ' -f 1 | sort --parallel=4 -n | uniq -i)"
+myip="127.0.0.1|192.168.1.1|192.168.1.222|192.168.1.70"
+list="$(rg "jndi|system.php|actuator|kerbynet|RadEditorProvider|telerikwebui|Telerik.Web.UI|phpMyAdmin|content-post.php|mide.php|phpunit|HEADER.md|/owa/auth/logon.aspx|main.installer.php|\?author=[0-9]|wp-login.php|logon|getuser|execute|passwd|Nikto|/\.env |nuclei|union|UNION|sql|conf|/[(w|x)]*shell|cmd|Admin|admin|cfg|boaform" "$log" | rg -v "$myip|DuckDuckBot|Adsbot|Googlebot|YandexBot|bingbot|SemrushBot|PetalBot" | cut -d ' ' -f 1,9 | rg '301|302|400|403|404|499' | cut -d ' ' -f 1 | sort --parallel=4 -n | uniq -i)"
 
 if [[ "$EUID" -ne 0 ]]; then
 printf "This script must be run as root! sudo $0 \n"
